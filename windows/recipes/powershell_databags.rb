ac8fa41a100bd08ea88ffd9112eba3c70e413167
Darko Mesaros <darko.subotica@gmail.com>
2016-09-14 17:16:05 +0100

    fixed quotes

--- windows/recipes/powershell_databags.rb
+++ windows/recipes/powershell_databags.rb
@@ -2,8 +2,7 @@ Chef::Log.info("******Testing powershell and environment variables ******")
 
 powershell_script "Get some data from databags n stuff" do
  guard_interpreter :powershell_script
- code <<-EOC
- 	echo "====== TESTING STUFF ======"
-	echo "{#node['myvar']}" >> c:/temp.txt
- EOC
+ code <<-EOH
+	echo '#{node['myvar']}' >> c:/temp.txt
+ EOH
 end
