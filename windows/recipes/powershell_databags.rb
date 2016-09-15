36ebc37025cd0358a7140682e4e10542a07377e1
Darko Mesaros <darko.subotica@gmail.com>
2016-09-15 10:43:52 +0100

    added variable

--- windows/recipes/powershell_databags.rb
+++ windows/recipes/powershell_databags.rb
@@ -3,6 +3,7 @@ Chef::Log.info("******Testing powershell and environment variables ******")
 powershell_script "Get some data from databags n stuff" do
  guard_interpreter :powershell_script
  code <<-EOH
-	echo '#{node['myvar']}' >> c:/temp.txt
+	$myvariable = '#{node['myvar']}'
+	echo $myvariable >> c:\tmp1.txt
  EOH
 end
