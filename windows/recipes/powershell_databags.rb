a3d7e60c3d38bf851350a264372f557d8a59c25a
darko-mesaros <darko.subotica@gmail.com>
2016-09-15 10:53:52 +0100

    fixed piping

--- windows/recipes/powershell_databags.rb
+++ windows/recipes/powershell_databags.rb
@@ -4,6 +4,6 @@ powershell_script "Get some data from databags n stuff" do
  guard_interpreter :powershell_script
  code <<-EOH
 	$myvariable = '#{node['myvar']}'
-	echo $myvariable >> c:\tmp1.txt
+	echo $myvariable >> c:/tmp1.txt
  EOH
 end
