a3e762b54a2eb293860044a8b7b182fb417ba49a
darko-mesaros <darko.subotica@gmail.com>
2016-09-21 16:27:37 +0100

    fixed ruby_block

--- windows/recipes/deploy_fromS3.rb
+++ windows/recipes/deploy_fromS3.rb
@@ -12,18 +12,18 @@ ruby_block 'envvarblock' do
   	query = Chef::Search::Query.new
   	app = query.search(:aws_opsworks_app, "deploy:true").first
   	# First log line
-  	Chef::Log.info "Deploying #{app["shortname"]}."
+  	#Chef::Log.info "Deploying #{app["shortname"]}."
   	s3region = app[0][:environment][:S3REGION]
-    s3bucket = app[0][:environment][:BUCKET]
-	s3folder = app[0][:environment][:FOLDER]
-    s3filename = app[0][:environment][:FILENAME]
-    pclocalpath = app[0][:environment][:LOCALPATH]
+    #s3bucket = app[0][:environment][:BUCKET]
+	#s3folder = app[0][:environment][:FOLDER]
+    #s3filename = app[0][:environment][:FILENAME]
+    #pclocalpath = app[0][:environment][:LOCALPATH]
 
     Chef::Log.info "***** #{s3region} *****"
-    Chef::Log.info "***** #{s3bucket} *****"
-    Chef::Log.info "***** #{s3folder} *****"
-    Chef::Log.info "***** #{s3filename} *****"
-    Chef::Log.info "***** #{pclocalpath} *****"
+    #Chef::Log.info "***** #{s3bucket} *****"
+    #Chef::Log.info "***** #{s3folder} *****"
+    #Chef::Log.info "***** #{s3filename} *****"
+    #Chef::Log.info "***** #{pclocalpath} *****"
 
   end
 end
