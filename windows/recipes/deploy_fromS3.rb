c844ae9c694eab74e34c60f584ac482892d21d2e
darko-mesaros <darko.subotica@gmail.com>
2016-09-22 12:36:38 +0100

    CHanged the query search

--- windows/recipes/deploy_fromS3.rb
+++ windows/recipes/deploy_fromS3.rb
@@ -15,19 +15,18 @@ ruby_block 'envvarblock' do
   	# First log line
   	#Chef::Log.info "Deploying #{app["shortname"]}."
   	apps.each do |app| 
-	  	#s3region = app[:environment][:S3REGION]
-	    #s3bucket = app[:environment][:BUCKET]
-		#s3folder = app[:environment][:FOLDER]
-	    #s3filename = app[:environment][:FILENAME]
-	    #pclocalpath = app[:environment][:LOCALPATH]
+	  	s3region = app[:environment][:S3REGION]
+	    s3bucket = app[:environment][:BUCKET]
+		s3folder = app[:environment][:FOLDER]
+	    s3filename = app[:environment][:FILENAME]
+	    pclocalpath = app[:environment][:LOCALPATH]
 
-	    #Chef::Log.info "***** #{s3region} *****"
-	    #Chef::Log.info "***** #{s3bucket} *****"
-	    #Chef::Log.info "***** #{s3folder} *****"
-	    #Chef::Log.info "***** #{s3filename} *****"
-	    #Chef::Log.info "***** #{pclocalpath} *****"
+	    Chef::Log.info "***** #{s3region} *****"
+	    Chef::Log.info "***** #{s3bucket} *****"
+	    Chef::Log.info "***** #{s3folder} *****"
+	    Chef::Log.info "***** #{s3filename} *****"
+	    Chef::Log.info "***** #{pclocalpath} *****"
 
-	    Chef::Log.info "***** #{app[:environment][:S3REGION]} *****"
 	end
   end
 end
