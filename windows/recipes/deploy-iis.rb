8da72e76f922eb3d9572746cc964f106327ce12e
Meszaros <dmeszaro@f45c89933365.ant.amazon.com>
2017-07-08 22:13:13 +0100

    removed application

--- windows/recipes/deploy-iis.rb
+++ windows/recipes/deploy-iis.rb
@@ -1,10 +1,8 @@
 app = search("aws_opsworks_app","deploy:true").first  
 app_dir = "c:\inetpub\wwwroot\#{app[:shortname]}"  
 
-application app_dir do
-	git app_dir do
-		repository app["app_source"]["url"]
-		revision app["app_source"]["revision"]
-	end
-	
+git app_dir do
+	repository app["app_source"]["url"]
+	revision app["app_source"]["revision"]
 end
+	
