429c2e16c559ffc84f8332bbc9946bc5a8df3356
Meszaros <dmeszaro@f45c89933365.ant.amazon.com>
2017-07-08 23:18:33 +0100

    added cleanup

--- windows/recipes/deploy-iis.rb
+++ windows/recipes/deploy-iis.rb
@@ -1,8 +1,14 @@
 app = search("aws_opsworks_app","deploy:true").first  
-app_dir = "c:\\inetpub\\wwwroot\\"  
+app_dir = "c:\\inetpub\\wwwroot"  
+
+# Remove files from inetpub
+powershell_script 'clear_inetpub' do
+	code <<-EOH 
+	Remove-Item #app_dir\* -recurse
+	EOH
+end
 
 git app_dir do
 	repository app["app_source"]["url"]
 	revision app["app_source"]["revision"]
 end
-	
